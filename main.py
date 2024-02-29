73d6141baa761412f66ac23a1ababace12a0d796
Tanchikc34 <tanya123full@gmail.com>
2024-02-29 20:58:15 +0700

    1

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import sys
-import pygame
+from random import randrange
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-# Свой курсор мыши
-class App:
+# Git и желтые окружности
+class Example(QMainWindow):
     def __init__(self):
-        self.pos = (0, 0)
-        pygame.init()
-        pygame.display.set_caption("Свой курсор мыши")
-        self.screen = pygame.display.set_mode((500, 500))
-        self.clock = pygame.time.Clock()
-        self.image = pygame.image.load("data/arrow.png")
-        pygame.mouse.set_visible(False)
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setFixedSize(800, 600)
+        self.setWindowTitle('Git и желтые окружности')
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == pygame.MOUSEMOTION:
-                    self.pos = event.pos
-                    print("Позиция мыши: ", event.pos)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
 
-            # Отрисовка фона
-            self.screen.fill(pygame.Color("black"))
-            self.screen.blit(self.image, self.pos)
-            pygame.display.update()
-            self.clock.tick(60)
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 204, 0))
+        d = randrange(5, 150)
+        qp.drawEllipse(randrange(50, 750), randrange(120, 510), d, d)
 
 
 if __name__ == '__main__':
-    app = App()
-    app.run()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
